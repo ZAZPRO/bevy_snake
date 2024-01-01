fccb173c8adc3869dee796cb2fee2cf81607a300
Daniil Mira <contact@daniilmira.com>
2024-01-01 13:25:55 +0100

    Add window icon

--- src/bin/snake.rs
+++ src/bin/snake.rs
@@ -1,6 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::{prelude::*, window::PresentMode};
+use std::io::Cursor;
+
+use bevy::{
+    prelude::*,
+    window::{PresentMode, PrimaryWindow},
+    winit::WinitWindows,
+};
 use bevy_particle_systems::ParticleSystemPlugin;
 use snake::libs::{
     animation::CustomAnimationPlugin,
@@ -22,6 +28,27 @@ use snake::libs::{
         start_menu::StartMenuPlugin,
     },
 };
+use winit::window::Icon;
+
+// Sets the icon on windows and X11
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window: Query<Entity, With<PrimaryWindow>>,
+) {
+    let primary_entity = primary_window.single();
+    let Some(primary) = windows.get_window(primary_entity) else {
+        return;
+    };
+    let icon_buf = Cursor::new(include_bytes!("../../build/windows/icon.png"));
+
+    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+        let image = image.into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary.set_window_icon(Some(icon));
+    };
+}
 
 fn main() {
     let mut app = App::new();
@@ -40,6 +67,7 @@ fn main() {
             }),
             ..default()
         }))
+        .add_systems(PreStartup, set_window_icon)
         .add_plugins(SchedulePlugin)
         .add_plugins(GameStatatesPlugin)
         .add_plugins(EventsPlugin)
